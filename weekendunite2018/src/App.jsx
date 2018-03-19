d9a4a3df38a2903c9cebc86ff23b0cbb2495eaf0
DBdiego <diego.de.buysscher@gmail.com>
2018-03-19 21:50:05 +0100

    added the theme song

--- weekendunite2018/src/App.jsx
+++ weekendunite2018/src/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'                    ;
+import ReactMusicPlayer     from 'react-sound'       ;
 import Routers              from './Components/Routers.jsx' ;
 import {Link}               from 'react-router-dom'         ;
 import logo                 from './logo.svg'               ;
@@ -7,18 +8,27 @@ import './App.css';
 
 class App extends Component {
 
+
   render() {
     return (
       <div className = 'App'>
         <header className = 'AppHeader'>
         	<img src={logo} className = 'App-logo' alt='logo' />
-			<ul>
-				<li><Link to='/home' ><button type='button'>EQUIPE</button></Link></li>
-				<li><Link to='/games'><button type='button'>JEUX</button></Link></li>
-				<li><Link to='/info' ><button type='button'>INFO PRATIQUES</button></Link></li>
-			</ul>
+  			<ul>
+  				<li><Link to='/home' ><button type='button'>EQUIPE</button></Link></li>
+  				<li><Link to='/games'><button type='button'>JEUX</button></Link></li>
+  				<li><Link to='/info' ><button type='button'>INFO PRATIQUES</button></Link></li>
+  			</ul>
         </header>
         <Routers/>
+        <ReactMusicPlayer 
+          url={'./HP-theme.mp3'} 
+          autoLoad={true} 
+          autoPlay={true} 
+          loop={true}
+          playStatus={ReactMusicPlayer.status.PLAYING}
+          volume={0}
+        />
       </div>
 
     );
